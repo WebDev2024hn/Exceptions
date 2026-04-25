69706844eb8ba5619535260224d5846a32723429
camaya-wq <candido.amaya@outlook.com>
2026-04-24 18:11:15 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// Muestra el mensaje en la consola
+Console.WriteLine("Ingrese primer numero:");
+double n1 = double.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine("Ingrese primer numero:");
+double n2 = double.Parse(Console.ReadLine() ?? "0");
+
+double division = n1 / n2;
+
+Console.WriteLine($"La division de {n1} y {n2} es: {division}");
+//Console.WriteLine("La division de " + n1 + " y " + n2 + "es: " + division);
